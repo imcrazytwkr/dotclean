1f7b52561178d1b187ff9c4437769f5e44576128
Denis Chernov <d3lusion4l.nom4d@gmail.com>
2026-08-08 12:51:20 +0300

    Updated nix flake
    It is possible to build dotclean for nixpkgs now!

--- flake.nix
+++ flake.nix
@@ -11,29 +11,69 @@
       inherit (nixpkgs.lib) genAttrs;
       supportedSystems = [
         "aarch64-darwin"
+        "aarch64-linux"
         "x86_64-darwin"
         "x86_64-linux"
       ];
-      forAllSystems = f: genAttrs supportedSystems (system: f system);
+      forAllSystems =
+        f:
+        genAttrs supportedSystems (
+          system:
+          f {
+            inherit system;
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
     in
     {
-      formatter = forAllSystems (
-        system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        pkgs.nixfmt
+      formatter = forAllSystems ({ pkgs, ... }: pkgs.nixfmt);
+
+      packages = forAllSystems (
+        { pkgs, ... }:
+        {
+          default = pkgs.buildGoModule {
+            pname = "dotclean";
+            version = self.shortRev or self.dirtyShortRev or "dev";
+            src = self;
+
+            vendorHash = "sha256-XNj4dHQko/ECAACrD7qcgNq5pgwm5hHqSKpkhdcBD/U=";
+
+            # Go and Make are already provided by buildGoModule macro
+            nativeBuildInputs = [ pkgs.pandoc ];
+
+            # Custom Make phases skip buildGoModule's go-test helpers.
+            doCheck = false;
+
+            buildPhase = ''
+              runHook preBuild
+              make build
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              make PREFIX=$out install
+              runHook postInstall
+            '';
+
+            meta = {
+              description = "A drop-in cross-platform reimplementation of macOS dot_clean";
+              homepage = "https://github.com/imcrazytwkr/dotclean";
+              license = pkgs.lib.licenses.asl20;
+              mainProgram = "dotclean";
+            };
+          };
+        }
       );
+
       devShells = forAllSystems (
-        system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
+        { pkgs, system }:
         {
           default = pkgs.mkShell {
-            buildInputs = [
-              pkgs.go
-              pkgs.gnumake
+            inputsFrom = [ self.packages.${system}.default ];
+            packages = [
+              pkgs.just
+              pkgs.nixd
             ];
             shellHook = ''
               PS1='\u@dotclean-dev:\w/ > '
